689503d48aa2172ecbe6ea693bb7fd8c53f126ff
andreyegor <andreevegor10000@gmail.com>
2021-02-24 23:10:20 +0700

    Small commit

--- main/multiple_prewiew.py
+++ main/multiple_prewiew.py
@@ -3,12 +3,14 @@ import csv
 
 import FictionBookLib
 import TextAnalyzerLib
-author = ['Аркадий']
+
+author = []  # [name,surname]
 compare_author = False
-way_to_file = "main/example/output/book.csv"
+way_to_file = r'main/example/output/book.csv'
 way_to_folder = r'main/example/multiple_example'
 columns_names = ['Author', 'Title',
                  'Lexical diversity', 'Mean word len', 'Mean sentence len', 'Commas per 1000 symbols']
+
 if __name__ == '__main__':
     list_of_files = listdir(way_to_folder)
     out_file = open(way_to_file, 'w', encoding='utf-8')
